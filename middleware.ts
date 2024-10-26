4f9d3d12559eee9202890d29ae71574ebbd0bc9b
Sushant Panwar <sushant.spanwar@gmail.com>
2024-10-26 15:44:35 +0530

    fix: protect all routes

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,11 @@
-import { NextResponse } from "next/server";
-
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/"]);
+const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
 
-export default clerkMiddleware((auth, request) => {
-  if (isProtectedRoute(request)) {
-    auth().protect();
+export default clerkMiddleware(async (auth, request) => {
+  if (!isPublicRoute(request)) {
+    await auth.protect();
   }
-
-  return NextResponse.next();
 });
 
 export const config = {
