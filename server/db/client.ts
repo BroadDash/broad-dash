b078cc1a158c19f91ce6b7d9ff4dbdca9de5b78a
Sushant Panwar <sushant.spanwar@gmail.com>
2024-10-27 14:25:47 +0530

    feat: add data access to fetch data

--- server/db/client.ts
+++ server/db/client.ts
@@ -4,15 +4,27 @@ import { db } from "@/lib/db";
 import { clientSchema } from "@/schema/client";
 
 export async function createClient(data: z.infer<typeof clientSchema>) {
-  console.log("creating client in db... ");
   try {
     const value = await db.client.create({
       data,
     });
-    console.log(value);
     return value;
   } catch (error) {
     console.error("Error details:", error);
     throw new Error("Failed to create client.");
+  }
+}
+
+export async function getClientDetails(id: string) {
+  try {
+    const value = await db.client.findUnique({
+      where: {
+        id,
+      },
+    });
+    return value;
+  } catch (error) {
+    console.error("Error details:", error);
+    throw new Error("Failed to find any client.");
   }
 }
